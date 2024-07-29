2083143c07bd307dfff09b0f3028348decb4c757
Ruslan Matkovsky <matkovsky.ruslan@gmail.com>
2024-07-30 01:44:51 +0200

    add nextjs for netilfy

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// eslint-disable-next-line
+export default async function (req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  if (url.pathname.includes('post')) {
+    url.pathname = '/about';
+    url.href = url.href.replace('/post', '/about');
+    return NextResponse.redirect(url);
+  }
+}
